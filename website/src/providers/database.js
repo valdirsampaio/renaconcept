af9811f547acb55d4e472fa620f727d50a0d6b35
Valdir Sampaio <valdirgomes.sjr@gmail.com>
2024-01-09 10:14:47 -0300

    feat: componentes iniciais

---
+++ website/src/providers/database.js
@@ -0,0 +1,12 @@
+import fashionStore from "../fashionStore.png"
+
+export const products = [
+    {
+        "id": 1,
+        "name": "",
+        "image": "",
+        "year": "",
+        "url": "",
+        "type": ""
+    },
+]
