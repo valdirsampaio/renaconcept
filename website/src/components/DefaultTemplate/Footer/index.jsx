268f6fdd702e99f1cd625e4294991ea8fbabcfef
Valdir Sampaio <valdirgomes.sjr@gmail.com>
2024-01-25 16:29:19 -0300

    feat: footer

--- website/src/components/DefaultTemplate/Footer/index.jsx
+++ website/src/components/DefaultTemplate/Footer/index.jsx
@@ -1,8 +1,7 @@
 import styles from "./style.module.scss";
 import React from "react"
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
-import { FaLocationDot } from "react-icons/fa6";
+import { FiPhone } from "react-icons/fi";
+import { IoIosMail } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logoRena from "../../../assets/logomarca/logo5.png";
@@ -15,31 +14,30 @@ export const Footer = () => {
             <div className={styles.footerContainer}>
                 <section className={styles.aboutRena}>
                     <img src={logoRena} alt="logoimg" className={styles.logoImg} />
-                    <p>Rena Concept é uma marca de roupas e fundada por Renata Albuquerque.
-                    </p>
-                    <p>@2024 Rena Concept LTDA - CNPJ: </p>
+                    <h4 className={styles.descriptionRena}>Rena Concept é uma marca de roupas fundada por Renata Albuquerque.
+                    </h4>
+                    <p className={styles.paragraph}>@ 2024 Rena Concept LTDA - CNPJ: </p>
                 </section>
                 <section className={styles.infosSection}>
                     <h3 className={styles.sectionTitle}>INSTITUCIONAL</h3>
-                    <Link>
+                    <Link className={styles.interactionButton}>
                         Quem Somos
                     </Link>
-                    <Link>
+                    <Link className={styles.interactionButton}>
                         Imprensa
                     </Link>
-                    <Link>
+                    <Link className={styles.interactionButton}>
                         Politicas de Privacidade
                     </Link>
-                    <Link>
+                    <Link className={styles.interactionButton}>
                         Trocas e Devoluções
                     </Link>
                 </section>
                 <section className={styles.contactSection}>
-                    <h3 className={styles.sectionTitle}><span>Entre em cont</span>ato</h3>
-                    <p className={styles.contactSectionContainer}><FaPhoneAlt className={styles.contactSectionIcon} /> <a target="_blank" href="tel:+558399322-6363" className={styles.contactSectionParagraph}>(83) 99322-6363</a></p>
-                    <p className={styles.contactSectionContainer}><FaInstagram className={styles.contactSectionIcon} /> <a target="_blank" href="https://www.instagram.com/toocoworking/" className={styles.contactSectionParagraph}>@toocoworking</a></p>
-                    <p className={styles.contactSectionContainer}><IoMdMail className={styles.contactSectionIcon} /> <a href="mailto:contato@soutoocoworking.com.br?subject=Contato por meio do site!" target="_blank" className={styles.contactSectionParagraph}>contato@soutoocoworking.com.br</a></p>
-                    <p className={styles.contactSectionContainer}><FaLocationDot className={styles.contactSectionIcon} /> <a target="_blank" className={styles.contactSectionParagraph}>Av. Rio Grande do Sul, 1345 - Estados, João Pessoa/PB</a></p>
+                    <h3 className={styles.sectionTitle}>CONTATO</h3>
+                    <p className={styles.contactParagraph}><FaInstagram className={styles.contactIcon} />@renaconcept</p>
+                    <p className={styles.contactParagraph}><FiPhone className={styles.contactIcon} />(81) 996266264</p>
+                    <p className={styles.contactParagraph}><IoIosMail className={styles.contactIcon} />renaconcept@contato.com.br</p>
                 </section>
             </div>
             <div className={styles.footerCopyright}>
